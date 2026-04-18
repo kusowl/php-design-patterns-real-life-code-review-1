2eb9f4fb0e134fdbe1392c18f9f59bb069b3a016
kusowl <kushal.saha@outlook.com>
2026-04-18 19:58:03 +0530

    create singleton config class with static proxy

---
+++ 1.start/src/Config/Config.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Config;
+class Config
+{
+    /**
+     * @var array<string, string> $values
+     */
+    private static array $values = [];
+    private static ?self $instance = null;
+
+    public static function
+    getValue(
+        string $key
+    ): string {
+        self::init();
+        return self::$values[$key] ?? "";
+    }
+
+    public static function init(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        echo "constructing..";
+        self::$values['db_host'] = 'localhost';
+        self::$values['db_name'] = 'code_review1';
+        self::$values['db_user'] = 'kushal';
+        self::$values['db_pass'] = 'pkrb';
+    }
+
+    private
+    function __clone(): void
+    {
+    }
+
+    private
+    function __wakeup(): void
+    {
+    }
+}
